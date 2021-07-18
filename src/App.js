696b4bf0675d61080b6820545aefe1db5eaf1350
Liam Stojanovic <liam.stojanovic@gmail.com>
2021-07-18 19:28:09 -0400

    add space component

--- src/App.js
+++ src/App.js
@@ -1,11 +1,12 @@
 import './App.css';
 import SpaceNavigation from './components/SpaceNavigation'
-
+import Space from './components/Space'
 function App() {
   return (
     <div className="App">
       <h1>Navigate between rooms...</h1>
       <SpaceNavigation />
+      <Space />
     </div>
   );
 }
