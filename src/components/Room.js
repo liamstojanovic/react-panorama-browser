febe4f12721d217a66fc7adccc94106acaaee301
Liam Stojanovic <liam.stojanovic@gmail.com>
2021-07-18 15:55:44 -0400

    add pannellum to pkg, room component

---
+++ src/components/Room.js
@@ -0,0 +1,36 @@
+import React, { Component } from 'react'
+
+export class Room extends Component {
+    constructor(props) {
+        super(props)
+
+        this.state = {
+            pannellumJson : [
+                {
+                    title: "Parking lot and Building",
+                    basePath: 'assets/img/IMG1539.jpg',
+                    autoLoad: true, 
+                    orientationOnByDefault: true,
+                },
+                {
+                    title: "Lobby",
+                },
+                {
+                    title: "Patient Rooms",
+                }
+            ]
+        }
+
+    }
+
+
+    render() {
+        return (
+            <div>
+                
+            </div>
+        )
+    }
+}
+
+export default Room
