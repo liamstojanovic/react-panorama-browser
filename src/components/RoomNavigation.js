11120d855bbdc4f7302f44ea21c46cb9bbc4bf20
Liam Stojanovic <liam.stojanovic@gmail.com>
2021-07-18 14:11:52 -0400

    swapped out arrow func with class component

--- src/components/RoomNavigation.js
+++ src/components/RoomNavigation.js
@@ -1,16 +1,53 @@
-import React, { useState } from 'react'
+import React, { Component } from 'react'
 
-const RoomNavigation = () => {
-    
-    const [roomId, setRoomId] = useState(0)
-
-    return (
-        <div>
-            <button onClick={() => setRoomId(roomId - 1)}>Previous room</button>
-            <button onClick={() => setRoomId(roomId + 1)}>Next room</button>
-            <p>Room id: {roomId}</p>
-        </div>
-    )
+export class RoomNavigation extends Component {
+    constructor(props) {
+        super(props)
+        this.state = {
+            roomId: 0
+        }
+        this.incrementRoomId = this.incrementRoomId.bind(this)
+        this.decrementRoomId = this.decrementRoomId.bind(this)
+    }
+    incrementRoomId () {
+        this.setState(state => ({
+            roomId: state.roomId + 1
+        }))
+    }
+    decrementRoomId () {
+        this.setState(state => ({
+            roomId: state.roomId - 1
+        }))
+    }
+    render() {
+        return (
+            <div>
+                <button onClick={this.decrementRoomId}>Previous room</button>
+                <button onClick={this.incrementRoomId}>Next room</button>
+                <h1>Current room ID: {this.state.roomId}</h1>
+            </div>
+        )
+    }
 }
 
 export default RoomNavigation
+
+
+
+
+// import React, { useState } from 'react'
+
+// const RoomNavigation = () => {
+    
+//     const [roomId, setRoomId] = useState(0)
+
+//     return (
+//         <div>
+//             <button onClick={() => setRoomId(roomId - 1)}>Previous room</button>
+//             <button onClick={() => setRoomId(roomId + 1)}>Next room</button>
+//             <p>Room id: {roomId}</p>
+//         </div>
+//     )
+// }
+
+// export default RoomNavigation
