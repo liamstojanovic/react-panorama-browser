77c1aa4f8e651390c4a5c805e3b842e229a19160
Liam Stojanovic <liam.stojanovic@gmail.com>
2021-07-18 19:18:56 -0400

    boilerplate, potentially unnecessary

---
+++ src/components/SpaceButton.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react'
+
+export class SpaceButton extends Component {
+    render() {
+        return (
+            <div>
+                
+            </div>
+        )
+    }
+}
+
+export default SpaceButton
