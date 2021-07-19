b8d6a9dc5fade7c95bb0b6000e211daa274bcca9
Liam Stojanovic <liam.stojanovic@gmail.com>
2021-07-19 14:26:38 -0400

    Removed boilerplate h1

--- src/App.js
+++ src/App.js
@@ -3,7 +3,6 @@ import SpaceNavigation from './components/SpaceNavigation';
 function App() {
   return (
     <div className="App">
-      <h1>Navigate between rooms...</h1>
       <SpaceNavigation />
     </div>
   );
