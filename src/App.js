5504403a8d9096cbd1e2be99fa565406a40d4c32
Liam Stojanovic <liam.stojanovic@gmail.com>
2021-07-19 11:10:22 -0400

    component hierarchy

--- src/App.js
+++ src/App.js
@@ -1,11 +1,12 @@
 import './App.css';
 // import SpaceNavigation from './components/SpaceNavigation'
 import Space from './components/Space'
+import SpaceNavigation from './components/SpaceNavigation';
 function App() {
   return (
     <div className="App">
       <h1>Navigate between rooms...</h1>
-      <Space />
+      <SpaceNavigation />
     </div>
   );
 }
