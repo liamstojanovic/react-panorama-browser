6e9e39d39d3022879bce4d192bd065ae69353ed5
Liam <liam.stojanovic@gmail.com>
2022-01-08 11:55:00 -0500

    separate spaces from space component

---
+++ src/shared/panorama.js
@@ -0,0 +1,17 @@
+export const spaces = [
+    {
+        title: "Parking lot and Building",
+        panorama: 'https://f002.backblazeb2.com/file/cw3-public/walker/img/IMG_1539.jpg',
+        autoLoad: true, 
+    },
+    {
+        title: "Lobby",
+        panorama: 'https://f002.backblazeb2.com/file/cw3-public/walker/img/IMG_1535.jpg',
+        autoLoad: true,
+    },
+    {
+        title: "Patient Rooms",
+        panorama: 'https://f002.backblazeb2.com/file/cw3-public/walker/img/IMG_1522.jpg',
+        autoLoad: true
+    }
+]
