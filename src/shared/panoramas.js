f174dd35027e5e1bd0dab83953193132178b8852
Liam <liam.stojanovic@gmail.com>
2022-01-16 16:20:42 -0500

    example

--- src/shared/panoramas.js
+++ src/shared/panoramas.js
@@ -3,19 +3,25 @@ export const spaces = [
         title: "Parking Lot and Building",
         panorama: 'https://f002.backblazeb2.com/file/cw3-public/walker/img/IMG_1539.jpg',
         autoLoad: true, 
-        slug: 'parking-lot-and-building',
+        slug: 'parking-lot-and-building'
     },
     {
         title: "Lobby",
         panorama: 'https://f002.backblazeb2.com/file/cw3-public/walker/img/IMG_1535.jpg',
         autoLoad: true,
-        slug: 'lobby',
+        slug: 'lobby'
     },
     {
         title: "Patient Rooms",
         panorama: 'https://f002.backblazeb2.com/file/cw3-public/walker/img/IMG_1522.jpg',
         autoLoad: true,
-        slug: 'patient-rooms',
+        slug: 'patient-rooms'
+    },
+    {
+        title: "Castillo's Man Cave",
+        panorama: "none",
+        autload: true,
+        slug: 'castillos-man-cave'
     }
 ]
 
