da26067ac4cc7e285e62e03257749d2efc17c513
Liam <liam.stojanovic@gmail.com>
2022-01-16 15:36:44 -0500

    unused import

--- src/App.js
+++ src/App.js
@@ -1,9 +1,6 @@
 import './App.css';
 import './styles/wdp.css'
 import React, { useState } from 'react';
-import Panorama from './components/Panorama/Panorama';
-import DirectNavigation from './components/Navigation/DirectNavigation';
-import IncrementalNavigation from './components/Navigation/IncrementalNavigation';
 import inputData from './shared/panoramas'
 import Navigation from './components/Navigation';
 
