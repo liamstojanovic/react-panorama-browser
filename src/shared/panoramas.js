625042fd115b4555ed9b34599f93e1a84ba7209a
Liam <liam.stojanovic@gmail.com>
2022-02-14 16:05:04 -0500

    style experimentation with >3 panoramas

--- src/shared/panoramas.js
+++ src/shared/panoramas.js
@@ -7,19 +7,34 @@
 
 export const spaces = [
     {
-        title: "Parking Lot and Building",
-        panorama: 'https://f002.backblazeb2.com/file/cw3-public/walker/img/IMG_1539.jpg',
-        slug: 'parking-lot-and-building'
+        title: "Forggensee",
+        panorama: 'https://upload.wikimedia.org/wikipedia/commons/5/58/Forggensee_Panorama_SK_0001.jpg',
+        slug: 'forggensee'
     },
     {
-        title: "Lobby",
-        panorama: 'https://f002.backblazeb2.com/file/cw3-public/walker/img/IMG_1535.jpg',
-        slug: 'lobby'
+        title: "Bödele Bregenzerwald",
+        panorama: 'https://upload.wikimedia.org/wikipedia/commons/1/11/B%C3%B6dele_Bregenzerwald_Panorama.jpg',
+        slug: 'Bodele-bregenzerwald'
     },
     {
-        title: "Patient Rooms",
-        panorama: 'https://f002.backblazeb2.com/file/cw3-public/walker/img/IMG_1522.jpg',
-        slug: 'patient-rooms'
+        title: "Longerich",
+        panorama: 'https://upload.wikimedia.org/wikipedia/commons/3/30/Longerich_%E2%80%93_360%C2%B0_Panorama_aus_150_m_H%C3%B6he_%E2%80%93_April_2021.jpg',
+        slug: 'longerich'
+    },
+    {
+        title: "DC People and Places 3780",
+        panorama: 'https://upload.wikimedia.org/wikipedia/commons/3/3f/2017.02.18_Equirectangular_DC_People_and_Places_3780_%2832132214284%29.jpg',
+        slug: 'dc-people-places-3780'
+    },
+    {
+        title: "Longerich",
+        panorama: 'https://upload.wikimedia.org/wikipedia/commons/3/30/Longerich_%E2%80%93_360%C2%B0_Panorama_aus_150_m_H%C3%B6he_%E2%80%93_April_2021.jpg',
+        slug: 'longerich'
+    },
+    {
+        title: "Longerich",
+        panorama: 'https://upload.wikimedia.org/wikipedia/commons/3/30/Longerich_%E2%80%93_360%C2%B0_Panorama_aus_150_m_H%C3%B6he_%E2%80%93_April_2021.jpg',
+        slug: 'longerich'
     }
 ]
 
