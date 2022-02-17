a78cb316d8ccac845bfe9281f377062d5755ee49
Liam <liam.stojanovic@gmail.com>
2022-02-17 11:09:04 -0500

    equirectangular panoramas

--- src/shared/panoramas.js
+++ src/shared/panoramas.js
@@ -7,14 +7,9 @@
 
 export const spaces = [
     {
-        title: "Forggensee",
-        panorama: 'https://upload.wikimedia.org/wikipedia/commons/5/58/Forggensee_Panorama_SK_0001.jpg',
-        slug: 'forggensee'
-    },
-    {
-        title: "Bödele Bregenzerwald",
-        panorama: 'https://upload.wikimedia.org/wikipedia/commons/1/11/B%C3%B6dele_Bregenzerwald_Panorama.jpg',
-        slug: 'Bodele-bregenzerwald'
+        title: "Ampleben Sternenhimmel",
+        panorama: 'https://upload.wikimedia.org/wikipedia/commons/0/04/Ampleben_Sternenhimmel_Panorama.jpg',
+        slug: 'Ampleben-Sternenhimmel'
     },
     {
         title: "Longerich",
@@ -25,16 +20,6 @@ export const spaces = [
         title: "DC People and Places 3780",
         panorama: 'https://upload.wikimedia.org/wikipedia/commons/3/3f/2017.02.18_Equirectangular_DC_People_and_Places_3780_%2832132214284%29.jpg',
         slug: 'dc-people-places-3780'
-    },
-    {
-        title: "Longerich",
-        panorama: 'https://upload.wikimedia.org/wikipedia/commons/3/30/Longerich_%E2%80%93_360%C2%B0_Panorama_aus_150_m_H%C3%B6he_%E2%80%93_April_2021.jpg',
-        slug: 'longerich'
-    },
-    {
-        title: "Longerich",
-        panorama: 'https://upload.wikimedia.org/wikipedia/commons/3/30/Longerich_%E2%80%93_360%C2%B0_Panorama_aus_150_m_H%C3%B6he_%E2%80%93_April_2021.jpg',
-        slug: 'longerich'
     }
 ]
 
